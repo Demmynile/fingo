98986b1da2f17bcd277c441367b00aaa12c06d30
demmynile <balogun80@gmail.com>
2025-01-20 05:02:20 +0000

    add new information

--- backend/db/tests/main_test.go
+++ backend/db/tests/main_test.go
@@ -1,13 +1,21 @@
-package db
+package db_test
 
 import (
+	"database/sql"
+	db "github/demmynile/fingo/db/sqlc"
 	"log"
+	"os"
 	"testing"
 )
 
+var testQuery *db.Queries
+
 func TestMain(m *testing.M){
-		conn , err := sql.open("postgres", "postgres://spicywords:Harbeedeymee_123@localhost:5432/fingreat_db?sslmode=disable")
+		conn , err := sql.Open("postgres", "postgres://spicywords:Harbeedeymee_123@localhost:5432/fingreat_db?sslmode=disable")
 		if err != nil{
 			log.Fatal("Could not connect to databae" , err)
 		}
+		testQuery = db.New(conn)
+
+		os.Exit(m.Run())
 }
--- backend/go.mod
+++ backend/go.mod
@@ -1,3 +1,5 @@
 module github/demmynile/fingo
 
 go 1.23.4
+
+require github.com/stretchr/testify v1.10.0 // indirect
